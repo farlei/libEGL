7aeb09275eabf94aea7f5e4b9d36ec77965ed5a2
Farlei Heinen <farleih@gmail.com>
2014-03-03 11:24:59 -0300

    Atualização para Visual Studio 2012

--- libEGL/include/library_version.h
+++ libEGL/include/library_version.h
@@ -1,7 +1,42 @@
 #pragma once
 // libegl4: Cabecalho para configuracao automatica das bibliotecas no Visual Studio
 
+#if _MSC_VER >= 1700
+   // this is Visual C++ 2012
+	#include "SDL.h"
+	#include "SDL_image.h"
+	#include "SDL_ttf.h"
+	#include "SDL_mixer.h"	
 
+	//SDL_gfx
+	#include "SDL_gfxPrimitives.h" //Graphic Primitives 
+	#include "SDL_rotozoom.h" //Rotozoomer 
+	#include "SDL_framerate.h" //Framerate control 
+	#include "SDL_imageFilter.h" //MMX image filters 
+	//#include "SDL_gfxBlitFunc.h" //Custom Blit functions 
+
+	#include "SDL_Collide.h"
+
+	#ifndef _DEBUG
+		#pragma message("RELEASE EGL4: added libs: sdl.lib sdlmain.lib sdl_image.lib (VS2012)")
+		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl.lib")
+		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdlmain.lib")
+		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl_image.lib")
+		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl_ttf.lib")
+		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl_gfx.lib")
+		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl_mixer.lib")
+		#pragma comment(lib, "..//..//libEGL//lib//NET2012//libegl.lib")
+	#else
+		#pragma message("DEBUG EGL4: added libs: sdl.lib sdlmain.lib sdl_image.lib (VS2012)")
+		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl.lib")
+		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdlmain.lib")
+		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl_image.lib")
+		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl_ttf.lib")
+		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl_gfx.lib")
+		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl_mixer.lib")
+		#pragma comment(lib, "..//..//libEGL//lib//NET2012//libegl_d.lib")
+	#endif	
+#else
 #if _MSC_VER >= 1600
    // this is Visual C++ 2010
 	#include "SDL.h"
@@ -19,7 +54,7 @@
 	#include "SDL_Collide.h"
 
 	#ifndef _DEBUG
-		#pragma message("RELEASE EGL4: added libs: sdl.lib sdlmain.lib sdl_image.lib (VS2010+)")
+		#pragma message("RELEASE EGL4: added libs: sdl.lib sdlmain.lib sdl_image.lib (VS2010)")
 		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl.lib")
 		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdlmain.lib")
 		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl_image.lib")
@@ -28,7 +63,7 @@
 		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl_mixer.lib")
 		#pragma comment(lib, "..//..//libEGL//lib//NET2010//libegl.lib")
 	#else
-		#pragma message("DEBUG EGL4: added libs: sdl.lib sdlmain.lib sdl_image.lib (VS2010+)")
+		#pragma message("DEBUG EGL4: added libs: sdl.lib sdlmain.lib sdl_image.lib (VS2010)")
 		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl.lib")
 		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdlmain.lib")
 		#pragma comment(lib, "..//..//libEGL//SDL//lib//sdl_image.lib")
@@ -76,4 +111,5 @@
 #else
 	#pragma message("ERRO: Versão do Visual Studio sem suporte automático! ")
 #endif 
+#endif
 #endif
---
+++ projetos/Tanque_2012/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "engcomp_glib.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	egl_inicializar(800,600,true);
+	egl_limite_framerate(120); // limita em 120 frames por segundo (o padrão é de 60 fps)
+
+	imagem tanque;
+	tanque.carregar("tanque.png");
+
+	TileMap mapa;
+	//mapa.inicializa("mapa.txt");
+	mapa.inicializaJSON("ruas.json","fundo");
+
+	TileMap crateras;
+	crateras.inicializaJSON("ruas.json","crateras");
+
+	float rot = 0;
+	float x = 300;
+	float y = 300;
+	float vel = 0.6;
+	float rad;
+	const float pi180 = (3.1416/180);
+
+	Interface gui;
+	gui.adicionaComponente(new Botao(0,650,450,"normal.png","clique.png","hover.png"));
+	gui.adicionaComponente(new Botao(1,650,500,"normal.png","clique.png","hover.png"));
+
+	ItemMenu* menu1 = new ItemMenu(2,"Texto do Item 1",20,450,200,35);
+	menu1->setarFonte("fonte.ttf",16,true);
+	ItemMenu* menu2 = new ItemMenu(3,"Texto do Item 2",20,490,200,35);
+	menu2->setarFonte("fonte.ttf",16);
+	gui.adicionaComponente(menu1);
+	gui.adicionaComponente(menu2);
+
+	int troca = 1;
+
+
+	imagem explosao;
+	explosao.carregar("explosao.png",0,0,32,32);
+	explosao.carregar("explosao.png",32,0,32,32);
+	explosao.carregar("explosao.png",32*2,0,32,32);
+	explosao.carregar("explosao.png",32*3,0,32,32);
+	explosao.carregar("explosao.png",32*4,0,32,32);
+	explosao.carregar("explosao.png",32*5,0,32,32);
+	explosao.carregar("explosao.png",32*6,0,32,32);
+
+	int delay = 0;
+	while(!key[SDLK_ESCAPE])
+	{
+		mapa.desenha();
+		crateras.desenha();
+		tanque.desenha_rotacionado(x,y,rot);
+
+		gui.desenha();
+		int btID = gui.verificaEventos(EGL_CLIQUE_DIR);
+		if(btID == 0) troca *= (-1);
+
+		int sair = gui.verificaEventosJanela(EGL_SAIR);
+		if(sair == 1) break;
+
+		// comandos que habilitam o painel de Debug
+		// e atualizam os valores
+		egl_depurar("X",x);
+		egl_depurar("Y",y);
+		egl_depurar("Rotacao Tanque",rot);
+		egl_depurar("Delay",delay);
+		egl_depurar("Clique",troca);
+
+		if(key[SDLK_LEFT])
+		{
+			rot+=vel;
+		}
+		if(key[SDLK_RIGHT])
+		{
+			rot-=vel;
+		}
+
+		if(key[SDLK_UP])
+		{
+			rad = rot*pi180;
+			x += cos(rad) * vel;
+			y += -sin(rad) * vel;
+		}
+		if(key[SDLK_DOWN])
+		{
+			rad = rot*pi180;
+			x -= cos(rad) * vel;
+			y -= -sin(rad) * vel;
+		}
+		
+		// linha com espessura (último parametro indica a espessura)
+		egl_linha(10,550,790,550,0,0,0,5);
+
+		// desenha um retangulo com as bordas arredondadas
+		egl_retangulo_arredondado(10,560,70,590,0,0,0,200);
+
+		explosao.desenha(400,300);
+
+		delay = egl_desenha_frame();
+	}
+
+	egl_finalizar();
+
+	return 0;
+}
